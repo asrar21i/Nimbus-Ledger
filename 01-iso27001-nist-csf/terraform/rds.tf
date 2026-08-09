906e3f566ab1edfacd5e4e2f6bad1529e7c7ede8
Asrar <asrarphoplenker7@gmail.com>
2026-08-09 16:27:12 +0530

    fix(terraform): add instance_class to rds and update compliance pipeline

--- 01-iso27001-nist-csf/terraform/rds.tf
+++ 01-iso27001-nist-csf/terraform/rds.tf
@@ -1,4 +1,9 @@
 resource "aws_db_instance" "main" {
+
+  engine                = "postgres"
+  instance_class        = "db.t3.micro"
+  allocated_storage     = 20
+  
   storage_encrypted  = true
   publicly_accessible = false
 
